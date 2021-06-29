f585f36205b0e1fed4ab204b5600d4006682da0b
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2021-06-29 10:35:54 -0300

    Adiciona estilização à tela de login

--- spmed-mobile/src/screens/login.js
+++ spmed-mobile/src/screens/login.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../services/api.js'
 import jwtDecode from 'jwt-decode';
@@ -36,26 +36,39 @@ export default class Login extends Component {
 
     render(){
         return (
-            <View>
-                <TextInput
-                placeholder='email'
-                placeholderTextColor='black'
-                keyboardType='email-address'
-                onChangeText={email => this.setState({email})}
-                />
+            <View style={styles.container}>
 
-                <TextInput
-                    placeholder='password'
-                    placeholderTextColor='black'
-                    secureTextEntry={true}
-                    onChangeText={senha => this.setState({senha})}
-                />
+                <View style={styles.DivImg}>
+                    <Image
+                        source={require('../../assets/img/logo-v2.png')}
+                        style={styles.ImgLogo}
+                    />
+                </View>
+                <View style={styles.FormLogin}>
+                    <TextInput
+                        style={styles.inputLogin}
+                        placeholderTextColor="#FFF"
+                        placeholder='Email'
+                        placeholderTextColor='#0B56E3'
+                        keyboardType='email-address'
+                        onChangeText={email => this.setState({email})}
+                    />
 
-                <TouchableOpacity
-                    onPress={this.fazerLogin}
-                >
-                    <Text>login</Text>
-                </TouchableOpacity>
+                    <TextInput
+                        style={styles.inputLogin}
+                        placeholder='Password'
+                        placeholderTextColor='#0B56E3'
+                        secureTextEntry={true}
+                        onChangeText={senha => this.setState({senha})}
+                    />
+
+                    <TouchableOpacity
+                        style={styles.btnLogin}
+                        onPress={this.fazerLogin}
+                    >
+                        <Text style={styles.btnLoginText}>login</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -65,7 +78,57 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  DivImg: {
+      backgroundColor: '#0B56E3',
+      height: 400,
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      flex: 3
+  },
+
+  ImgLogo: {
+      width: 250,
+      height: 250
+  },
+
+  FormLogin: {
+      flex: 2,
+      borderWidth: 1,
+      borderColor: '#0D21FC',
+      paddingLeft: 50,
+      paddingTop: 40
+  },
+
+  inputLogin: {
+      borderBottomWidth: 1,
+      borderBottomColor: "#0D21FC",
+      width: 250,
+      marginBottom: 25,
+      paddingBottom: 2,
+      textAlign: 'center',
+      color:"#000"
+  },
+
+  btnLogin: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: 30,
+    width: 175,
+    backgroundColor: '#0B56E3',
+    borderColor: '#0B56E3',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginLeft: 40,
+    marginTop: 30
   },
+
+  btnLoginText: {
+    fontSize: 14,
+    color: '#FFF',
+    textTransform: 'capitalize'
+}
 });
